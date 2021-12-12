9e3628f7d13a9cf49ad18eeb7b12da155bd40d22
priyalsavsani <priyalsavsani33@gmail.com>
2021-12-12 16:03:17 +0530

    Add files via upload

---
+++ Android-Barcode-Reader/README.md
@@ -0,0 +1,8 @@
+# Android Barcode Reader
+This is a simple Android barcode reader developed with ZXing.
+
+## Blog
+[How to implement a simple barcode scan application on Android][1]
+
+
+[1]:http://www.codepool.biz/ocr-barcode-twain/how-to-implement-a-simple-barcode-scan-application-on-android.html
---
+++ Android-Barcode-Reader/src/com/dynamsoft/barcodereader/BarcodeReader.java
@@ -0,0 +1,45 @@
+package com.dynamsoft.barcodereader;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Display;
+import android.widget.FrameLayout;
+
+public class BarcodeReader extends Activity {
+    private CameraPreview mPreview;
+    private CameraManager mCameraManager;
+    private HoverView mHoverView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		Display display = getWindowManager().getDefaultDisplay();
+		mHoverView = (HoverView)findViewById(R.id.hover_view);
+		mHoverView.update(display.getWidth(), display.getHeight());
+		
+		mCameraManager = new CameraManager(this);
+        mPreview = new CameraPreview(this, mCameraManager.getCamera());
+        mPreview.setArea(mHoverView.getHoverLeft(), mHoverView.getHoverTop(), mHoverView.getHoverAreaWidth(), display.getWidth());
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+        
+        getActionBar().hide();
+	}
+	
+	@Override
+    protected void onPause() {
+        super.onPause();
+        mPreview.onPause();
+        mCameraManager.onPause(); 
+    }
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		mCameraManager.onResume();
+		mPreview.setCamera(mCameraManager.getCamera());
+	}
+}
